6318e1d91685c072b825508a1d5fced5bc959f87
Jesus <j@pseudo.cc>
2020-12-25 15:36:53 -0800

    clearer default values for getlist

--- pkg/cmd/domains.go
+++ pkg/cmd/domains.go
@@ -30,10 +30,10 @@ func NewCmdDomainsGetList() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVar(&opts.ListType, "listtype", "", "Possible values are ALL, EXPIRING, or EXPIRED. Default Value: ALL")
+	cmd.Flags().StringVar(&opts.ListType, "listtype", "ALL", "Possible values are ALL, EXPIRING, or EXPIRED")
 	cmd.Flags().StringVar(&opts.SearchTerm, "searchterm", "", "Keyword to look for in the domain list")
-	cmd.Flags().StringVar(&opts.Page, "page", "", "Page to return Default Value: 1")
-	cmd.Flags().StringVar(&opts.PageSize, "pagesize", "", "Page to return Default Value: 1")
+	cmd.Flags().StringVar(&opts.Page, "page", "1", "Page to return")
+	cmd.Flags().StringVar(&opts.PageSize, "pagesize", "20", "Number of domains to be listed on a page. Minimum value is 10, and maximum value is 100")
 	cmd.Flags().StringVar(&opts.SortBy, "sortby", "", "Possible values are NAME, NAME_DESC, EXPIREDATE, EXPIREDATE_DESC, CREATEDATE, CREATEDATE_DESC")
 
 	return cmd
