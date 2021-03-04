40d22fb77ea3bacafb28782ba0b7c20562208f3d
Jesus <j@pseudo.cc>
2021-03-04 16:50:13 +0100

    Detect output type
    Disable JSON colorization if piping output

--- pkg/cmd/response.go
+++ pkg/cmd/response.go
@@ -3,19 +3,31 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/tidwall/pretty"
 )
 
 func printResults(v interface{}) {
-	// b, err := json.Marshal(v)
-	b, err := json.MarshalIndent(v, "", "    ")
-	if err != nil {
-		panic(err)
-	}
+	if fileInfo, _ := os.Stdout.Stat(); (fileInfo.Mode() & os.ModeCharDevice) != 0 {
+		b, err := json.MarshalIndent(v, "", "\t")
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Fprintln(
-		rootCmd.OutOrStdout(),
-		string(pretty.Color(b, nil)),
-	)
+		fmt.Fprintln(
+			rootCmd.OutOrStdout(),
+			string(pretty.Color(b, nil)),
+		)
+	} else {
+		b, err := json.Marshal(v)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintln(
+			rootCmd.OutOrStdout(),
+			string(b),
+		)
+	}
 }
